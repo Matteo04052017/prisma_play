3e3331cb5cb5dd1d4682969aae580e23637312ac
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2020-11-23 10:05:44 +0100

    updates

--- roles/client-automation/code/compress.py
+++ roles/client-automation/code/compress.py
@@ -8,17 +8,20 @@ def make_tarfile(output_filename, source_dir):
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 rootdir = ""
+outdir = ""
 ## parse arguments
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "r:", ["rootdir="])
+    opts, args = getopt.getopt(sys.argv[1:], "r:o:", ["rootdir=", "outdir="])
 
 except getopt.GetoptError:
     print("Please provide proper arguments.")
-    print("Usage: $python compress.py --rootdir=<rootdir>")
+    print("Usage: $python compress.py --rootdir=<rootdir> --outdir=<outdir>")
     sys.exit(2)
 for opt, arg in opts:
     if opt in ("-r", "--rootdir"):
         rootdir = arg
+    if opt in ("-o", "--outdir"):
+        outdir = arg
 
 list_subfolders_with_paths = []
 for f in os.scandir(rootdir):
@@ -26,10 +29,10 @@ for f in os.scandir(rootdir):
         list_subfolders_with_paths.append(f.path)
 
 for directory in list_subfolders_with_paths:
-    tar_name = directory + "/" + os.path.basename(directory) + ".tar.gz"
+    tar_name = outdir + "/" + os.path.basename(directory) + ".tar.gz"
     for f in os.scandir(directory):
         if (f.is_dir() and "events" in f.path):
             tar_name = directory + "/" + os.path.basename(directory) + "_with_events.tar.gz"
     print("make tar of diretory " + str(directory) + " with name: " + tar_name)
     make_tarfile(tar_name, directory)
-    shutil.rmtree(directory)
+    #shutil.rmtree(directory)
